44522db206df4ace5a91043f022cc5ce41ff2fce
Sergey <inboxshow1@gmail.com>
2022-11-28 23:44:06 +0300

    Update script.js
    add object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Create!")
+let numFilms = ''
+let questionOne = ''
+let questionTwo = ''
+
+let personalMov = {
+    count: "",
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
