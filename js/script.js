814b0b4223160a8421065d491a98a558bf17da5e
Sergey <inboxshow1@gmail.com>
2022-12-01 23:56:33 +0300

    Update script.js
    add btn perem

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ let numFilms = ''
 let inpQuestionOne = document.querySelector('.inpQsOne').value
 console.log(inpQuestionOne.innerHTML)
 let inpQuestionTwo = document.querySelector('.inpQsTwo')
+console.log(inpQuestionTwo);
+let btnOne = document.querySelector('.btn_1')
+let btnTwo= document.querySelector('.btn_2')
  
 let personalMov = {
     count: "",
@@ -13,3 +16,6 @@ let personalMov = {
     privat: false
 }
 
+btnOne.addEventListener("click",()=>{
+
+})
