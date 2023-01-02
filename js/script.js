9f51fedc98a9ea5775b25d683b74cf87fc03dba3
Sergey <inboxshow1@gmail.com>
2023-01-02 23:22:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,115 +1,31 @@
-'use strict'
+const formTodo = document.querySelector('.formTodo')
+const inpForm = document.querySelector('.inpForm')
 
-console.log("WORK")
-let container = document.querySelector('.todo__container')
+formTodo.addEventListener('submit', function (event) {
 
-let inpTodo = document.querySelector('.inpTodo')
+    event.preventDefault()
+    console.log('submit')
 
-let btnAddTodo = document.querySelector('.btnAddTodo')
+    //take text iput
+    const inpText = inpForm.value
 
-let arrTasks = []
-
-//create todo block
-let divDo = document.createElement('div')
-divDo.classList.add('divTodo')
-
-
-//this task in arr tasks
-const objNewTask = {
-    id: Date.now(),
-    text: '',
-    done: false
-}
-
-console.log(objNewTask)
-
-getLockalStorage()
+    //add text to the page
+    const textTodo = `
+    <li class="list-group-item d-flex justify-content-between task-item">
+    <span class="task-title"></span>
+    <div class="task-item__buttons">
+        <button type="button" data-action="done" class="btn-action">
+            <img src="./img/tick.svg" alt="Done" width="18" height="18">
+        </button>
+        <button type="button" data-action="delete" class="btn-action">
+            <img src="./img/cross.svg" alt="Done" width="18" height="18">
+        </button>
+    </div>
+    </li>`
 
 
-//check inp + color
-inpTodo.addEventListener("click", () => {
-    if (inpTodo.focus) {
-        inpTodo.style.backgroundColor = '#003153'
-    } else { inpTodo.style.backgroundColor = '#3E5F8A' }
 })
 
-//check inp value
-btnAddTodo.addEventListener("click", () => {
-    if (inpTodo.value == '') {
-
-        inpTodo.style.backgroundColor = 'rgb(67, 175, 175)'
-    }
-})
-
-//add todo
-btnAddTodo.onclick = () => {
-    if (inpTodo.value == '') {
-
-        return
-    }
-
-
-    container.append(divDo)
-
-    divDo.innerHTML = inpTodo.value
-
-    objNewTask.text = inpTodo.value
-
-    addTask()
-    saveToLockalStorage()
-
-
-    inpTodo.value = ''
-
-}
-
-console.log(arrTasks)
-
-if (localStorage.getItem('keyLockal')) {
-    divDo.innerHTML = objNewTask.text
-}
-
-function addTask(event) {
-
-    // event.preventDefault()
-
-    //take object task for input
-
-    //add object task in 
-    arrTasks.push(objNewTask)
-
-    console.log(arrTasks)
-
-    //make markuup for the task
-}
-//save LoskalStorage
-function saveToLockalStorage() {
-    localStorage.setItem('keyLockal', JSON.stringify(arrTasks))
-}
-
-//get lockalStorage
-function getLockalStorage() {
-    localStorage.getItem('keyLockal')
-    console.log(localStorage.getItem('keyLockal'))
-
-    //retrieve data from storage as an array
-    arrTasks = JSON.parse(localStorage.getItem('keyLockal'))
-
-    container.append(divDo)
-    divDo.innerHTML = objNewTask.text
-}
-
-// arrTasks.forEach((task)=>{
-
-//     console.log(task)
-
-//     for(let i in task){
-//         console.log(i)
-//     }
-
-//     divDo.innerHTML = tas
-
-// })
 
 
 
