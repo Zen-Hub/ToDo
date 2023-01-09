4186cc9bcfcb53c7cec64e4e444dd349fc3d523c
Sergey <inboxshow1@gmail.com>
2023-01-09 23:57:44 +0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/screens/Home/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +11,4 @@ root.render(
     <Home />
   </React.StrictMode>
 );
+
