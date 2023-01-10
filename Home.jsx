c103cf16bba4a79ba10efa89a34931d07b43fdfb
Sergey <inboxshow1@gmail.com>
2023-01-10 23:26:24 +0300

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Todoitem from './Todoitem'
+
+
+const todos = [
+  {
+    title: 'Finish the essay collaboration',
+    isCompleted: false,
+  },
+  {
+    title: 'Read next chapter of the book',
+    isCompleted: false
+  },
+  {
+    title: 'Send the finished assignment',
+    isCompleted: false
+  }
+]
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='bg'>
+      <Todoitem todo = {itemTodo}/>
     </div>
+
+
   )
 }
 
