5fef570ed1ccc9a60d386517ca229dce5bc186c4
Sergey <inboxshow1@gmail.com>
2023-01-15 23:31:09 +0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -8,7 +8,7 @@ import Home from './components/screens/Home/Home'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ToDoitem />
   </React.StrictMode>
 );
 
