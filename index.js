dd709d5edca4fd7cd5cfdf535708b2acefb2f1ed
Sergey <inboxshow1@gmail.com>
2023-01-25 23:16:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 let inpTask = document.querySelector('.inpTask');
 let buttonAdd = document.querySelector('.btnAdd');
-let liAllTask = document.querySelector('.liAllTask');
+let ulTodo = document.querySelector('.ulTodo')
 
+//ловим клик и запускаем функцию
+buttonAdd.addEventListener('click', AddTaskfunс)
 
-buttonAdd.addEventListener('click', function () {
-    console.log("click")
+//Добавили задачу
+function AddTaskfunс() {
+  //Текст который мы ввели в инпут
+  const inpText = inpTask.value
 
-    //Текст который мы ввели в инпут
-    const inpText = inpTask.value
+  //Формируем задачу для вывода на экран
+  const taskHTML = `
+      <li class="liAllTask">
+          <div class="taskText">${inpText}</div>
+          <button class="btnDone btnstyle" data-action = 'done'>done</button>
+          <button class="btnDelete btnstyle" data-action = 'delete'>delete</button>
+      </li>
+      `
 
-    //Формируем задачу для вывода на экран
-    const taskHTML = `
-        <li class="all-task">
-            <span class="task-text">${inpText}</span>
-            <button class="taskDone"></button>
-            <button class="taskDelete"></button>
-        </li>
-        `
+  //Выводим на экран(вставляем в HTML)
+  ulTodo.insertAdjacentHTML('beforeend', taskHTML)
 
-    //Выводим на экран
-    liAllTask.insertAdjacentHTML('beforeend',taskHTML)
+  //Очистим инпут
+  inpTask.value = ''
 
-    //Очистим инпут
-    inpTask.value = ''
+  //вернули фокус в инпут
+  inpTask.focus()
+}
 
-    //вернули фокус в инпут
-    taskInput.focus()
-    
-})
+//Удалили задачу 
+ulTodo.addEventListener('click', deleteTaskFun)
+
+function deleteTaskFun(event) {
+
+  if (event.target.dataset.action == 'delete') {
+    console.log('DEL')
+
+    //смотрим родителя с тегом <li> или его классом
+    const parent = event.target.closest('.liAllTask')
+    console.log(parent)
+    //удаляем родителя
+    parent.remove()
+
+  }
+}
+//insertAdjacentHTML вставит заданный элемент в HTML
+//focus() ставит фокус в заданную область
+//target.dataset находит по атрибуту
+//closest ищет родителя по классу или тегу
+//remove() удалит тег 
